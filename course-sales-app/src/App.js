b500a6aaf99f07e1934d2e773f39fe846d329692
Hassaan Ali <58141711+hassaanali723@users.noreply.github.com>
2021-07-25 02:01:02 +0500

    Completed

--- course-sales-app/src/App.js
+++ course-sales-app/src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Coursesales from './Coursesales';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+
+
+  render(){
+    var courses = [
+      {name:'React',price:1000},
+      {name:'Angular',price:2000},
+      {name:'Django',price:3000},
+      {name:'Flask',price:4000}
+    ];
+  
   return (
     <div className="App">
      <h1>Course Sales App</h1>
+     <Coursesales items = {courses}  />
     </div>
   );
+}
 }
 
 export default App;
